2369eba0f82a5fee4743f3cbb0bcf6dc31760546
sodipe <98396909+so-dipe@users.noreply.github.com>
2023-12-28 04:28:53 +0000

    small app

---
+++ inference.py
@@ -0,0 +1,23 @@
+from transformers import pipeline, AutoTokenizer
+from optimum.intel import OVModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = "<PAD>"
+
+model = OVModelForCausalLM.from_pretrained("assets/gpt2")
+
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+def generate_next_move(previous_moves):
+    if len(previous_moves) > 0:
+        num_moves = len(previous_moves.split(' '))
+        num_tokens = len(tokenizer(previous_moves)['input_ids'])
+        moves = generator(previous_moves, max_length=(num_tokens + 3))
+        moves = moves[0]['generated_text'].split(' ')
+        next_move = moves[num_moves]
+        return next_move
+    else:
+        return "must enter first move"
+
+
+# print(generate_next_move("d4"))
---
+++ interface.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from inference import generate_next_move
+
+st.title("Chess Move Generator")
+
+previous_moves = st.text_input("Enter previous chess moves (e.g., d4 d5 ...):")
+
+if st.button("Generate Next Move"):
+    if previous_moves:
+        next_move = generate_next_move(previous_moves)
+        st.success(f"Next move: {next_move}")
+    else:
+        st.warning("Please input a move.")
