003ac53568404b3f636cd51e302f9cf0905efc25
sodipe <98396909+so-dipe@users.noreply.github.com>
2023-12-29 02:13:25 +0000

    dir create

--- google_storage.py
+++ google_storage.py
@@ -1,14 +1,18 @@
+import os
 from google.cloud import storage
 from st_secrets import load_secrets
 
 load_secrets()
 
 def download_blobs(bucket_name, prefix='', local_directory=''):
+    # Ensure the local directory exists or create it if it doesn't
+    if not os.path.exists(local_directory):
+        os.makedirs(local_directory)
+
     client = storage.Client.from_service_account_json('service_account_key.json')
     bucket = client.get_bucket(bucket_name)
 
     blobs = bucket.list_blobs(prefix=prefix)
-
 
     for blob in blobs:
         if blob.name.endswith('/'):  # Check if it's a "folder" (blob with a trailing '/')
@@ -20,8 +24,8 @@ def download_blobs(bucket_name, prefix='', local_directory=''):
     return 0
 
 
-download_blobs(
-    'sodipe-models', 
-    prefix='gpt2/', 
-    local_directory='assets/gpt2/'
-)
+# download_blobs(
+#     'sodipe-models', 
+#     prefix='gpt2/', 
+#     local_directory='assets/gpt2/'
+# )
